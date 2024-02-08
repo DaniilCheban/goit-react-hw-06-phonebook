248c986cd602c854e92caa92a2511ac6b3d0c8f8
DaniilCheban <danyacheban@icloud.com>
2024-02-08 16:31:53 +0200

    hw 6.1

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,19 @@
 import { contactsReducer } from './contactsSlise';
 import { configureStore } from '@reduxjs/toolkit';
 
-import { persistStore, persistReducer } from 'redux-persist';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+
 import storage from 'redux-persist/lib/storage';
+
 const persistConfig = {
   key: 'root',
   storage,
@@ -12,6 +23,12 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, contactsReducer);
 export const store = configureStore({
   reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
